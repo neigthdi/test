00d351bd8e7196b22a0eb60fcca6b2bbf9ad505b
panwj <panwj@huidiancloud.com>
2025-07-22 16:29:18 +0800

    t

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,24 @@
 import { fileURLToPath, URL } from 'node:url'
 import nodeExternals from 'rollup-plugin-node-externals'
+import viteCompression from 'vite-plugin-compression'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import glsl from 'vite-plugin-glsl'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  vite: {
+    plugins: [
+      viteCompression({
+        algorithm: 'gzip', // 或 'brotliCompress'
+        ext: '.gz', // 生成的文件扩展名
+        threshold: 10240, // 仅压缩大于 10KB 的文件
+      }),
+    ],
+    build: {
+      assetsInlineLimit: 4096, // 小图片转为 Base64（默认 4KB）
+    },
+  },
   plugins: [
     vue(),
     glsl(), 
